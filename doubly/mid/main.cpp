2c029325e5106664836f0b79a4c7153f382e74e9
rrraine <lorraineequezada@gmail.com>
2024-10-09 12:01:37 +0800

    Create main.cpp

---
+++ doubly/mid/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+/*
+ linked list with sentinels 
+  implement methods: addHead(int num), addTail(int num), removeHead(),removeTail()
+ reset() - reset ra nimong linked list imo i deallocate tanang nodes
+ int getMiddle() imo i implement na dili ka mugamit sa size 
+    if even ang total nodes sa list kay i return nimo ang average sa duha ka middle, if odd kay return ra nimong middle
+void addAt() doubly with sentinels 
+    access from head if pos <= ceil(size/2.0) nya tail if mas duol sa tail
+*/
